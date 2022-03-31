c90ae2c9cb1af090202cd05d683202441d4d6977
hariprasad36825 <hariprasad36825@gmail.com>
2022-03-31 18:35:08 +0530

    modifed procfile

--- chatify/asgi.py
+++ chatify/asgi.py
@@ -15,7 +15,7 @@ from django.core.asgi import get_asgi_application
 
 import chatapp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatify.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
