ea725055b3f0ff8e95d3ec1ded64f71d9a60cdec
hariprasad36825 <hariprasad36825@gmail.com>
2022-03-31 19:05:21 +0530

    manage.py get changed

--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chatify.settings'
+import django
+django.setup()
 
 def main():
     """Run administrative tasks."""
